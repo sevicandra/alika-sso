16e36edd0d8fbbc3289461bc25788b8659d7570d
sevicandra <sevicandra4@gmail.com>
2026-07-07 10:57:49 +0700

    Add limit=0 parameter to GetKeluargaByNip API call
    Add query parameter limit=0 to the GetKeluargaByNip endpoint to control the API response. Also remove trailing whitespace.

--- src/services/kemenkeu.service.ts
+++ src/services/kemenkeu.service.ts
@@ -144,7 +144,7 @@ export class KemenkeuService {
       }
 
       const response = await axios.get(
-        `${ServiceKemenkeuConfig.BASE_URI}/hris/keluarga/Riwayat/GetKeluargaByNip/${nip}`,
+        `${ServiceKemenkeuConfig.BASE_URI}/hris/keluarga/Riwayat/GetKeluargaByNip/${nip}?limit=0`,
         {
           headers: {
             Authorization: `Bearer ${await this.getAccessToken()}`,
